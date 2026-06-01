12d4430f020439ec83e3d6a775a18506b67882d3
kaueteixeira-dell <kauetei357@gmail.com>
2026-06-01 11:32:44 -0300

    FEAT 3: Correção(A feat do carrinho foi alterada)

--- src/components/cart/CartSummary.vue
+++ src/components/cart/CartSummary.vue
@@ -1,12 +1,1 @@
-<script setup>
 // Este arquivo é um componente Vue que representa o último resumo do carrinho de compras, exibindo o total geral e um botão para finalizar a compra.
-
-</script>
-
-<template>
-
-</template>
-
-<style scoped>
-
-</style>
